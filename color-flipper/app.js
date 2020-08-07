e68f3efe7d6040ea270810a957a3e0794e3b6063
namiranda <naylenmiranda@gmail.com>
2020-08-07 20:05:19 -0300

    agrego generacion random de colores hex

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -1,9 +1,16 @@
 const btn = document.querySelector("button");
 const background = document.getElementsByClassName("color")[0];
 
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 
 function changeBgColor() {
-    background.style.background = "yellow";
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        let num = Math.floor(Math.random() * hex.length)
+        color += hex[num];
+    }
+    background.style.background = color;
+    btn.style.background = color;
 }
 
 btn.addEventListener("click", changeBgColor);
