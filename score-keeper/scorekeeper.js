8dae8e76ea040c85d2958d23cdfcee213f354c45
namiranda <naylenmiranda@gmail.com>
2020-08-09 19:01:01 -0300

    agrego gameOver

--- score-keeper/scorekeeper.js
+++ score-keeper/scorekeeper.js
@@ -4,22 +4,38 @@ const displayP1 = document.querySelector("#displayP1");
 const displayP2 = document.querySelector("#displayP2");
 
 const maxScore = 5;
+let gameOver = false;
 
 let scoreP1 = 0;
 let scoreP2 = 0;
 
-function addScoreP1(){
-    if(scoreP1 < maxScore){
+function addScoreP1() {
+    if (!gameOver) {
         scoreP1++;
     }
+    if (scoreP1 === maxScore) {
+        gameOver = true;
+    }
+    console.log(gameOver);
     displayP1.textContent = scoreP1;
 }
-function addScoreP2(){
-    if(scoreP2 < maxScore){
+
+function addScoreP2() {
+    if (!gameOver) {
         scoreP2++;
     }
+    if (scoreP2 === maxScore) {
+        gameOver = true;
+    }
+
+    console.log(gameOver);
     displayP2.textContent = scoreP2;
 }
 
 buttonP1.addEventListener("click", addScoreP1);
 buttonP2.addEventListener("click", addScoreP2);
+
+//TODO:
+//Implementar reset
+//Parar cuando uno llegue al maximo
+//Implementar el ingreso por teclado
