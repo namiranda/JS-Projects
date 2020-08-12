15cb31850b38438aabfe47ea6bdf4a35189a8af1
namiranda <49685356+namiranda@users.noreply.github.com>
2020-08-12 15:57:59 -0300

    agrego cambio de color en el puntaje del ganador

--- score-keeper/scorekeeper.js
+++ score-keeper/scorekeeper.js
@@ -16,6 +16,7 @@ function addScoreP1() {
     }
     if (scoreP1 === maxScore) {
         gameOver = true;
+        displayP1.classList.add("winner");
     }
     console.log(gameOver);
     displayP1.textContent = scoreP1;
@@ -27,6 +28,7 @@ function addScoreP2() {
     }
     if (scoreP2 === maxScore) {
         gameOver = true;
+        displayP2.classList.add("winner");
     }
 
     console.log(gameOver);
@@ -37,7 +39,9 @@ function reset(){
     scoreP1 = 0;
     scoreP2 = 0;
     displayP1.textContent = scoreP1;
+    displayP1.classList.remove("winner");
     displayP2.textContent = scoreP2;
+    displayP2.classList.remove("winner");
     gameOver = false;
 }
 
