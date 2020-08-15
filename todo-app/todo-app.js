4e53070d28d291a8f8d3544dad1805035f8fc495
namiranda <49685356+namiranda@users.noreply.github.com>
2020-08-15 01:12:24 -0300

    agrego contador de todo's y checkboxes funcionando

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,15 +1,38 @@
 const newTodo = document.querySelector("#new");
 const todoSection = document.querySelector("section");
+const counterDisplay = document.querySelector("#counter");
+let todoCheckbox;
+
 let todos = []; 
+let counter = 0;
 
 function createTodo(){
     console.log("new todo");
     let todo = newTodo.value;
     console.log(todo);
-    todos.push('<div><input type="checkbox" name="" id="">' + todo + ' <button>remove</button></div>');
+    todos.push('<div><input type="checkbox" class="todos" name="" id="">' + todo + ' <button>remove</button></div>');
     console.log(todos);
     console.log(todos.length);
     todoSection.innerHTML += todos[todos.length-1];
     newTodo.value = "";
+    counter++;
+    updateDisplayCounter();
+    todoCheckbox = document.querySelectorAll(".todos");
+    todoCheckbox.forEach(checkbox => {
+        checkbox.addEventListener("change", function(){
+
+            if(checkbox.checked){
+                counter--;
+            }else counter++;
+            updateDisplayCounter();
+        })
+    });
 }
-newTodo.addEventListener("change", createTodo);  
+
+function updateDisplayCounter(){
+    counterDisplay.textContent = counter;
+}
+
+newTodo.addEventListener("change", createTodo);
+
+
