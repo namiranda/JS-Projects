d1a0251f4093fcb733e226597b9f0e12a00b1fc3
namiranda <49685356+namiranda@users.noreply.github.com>
2020-08-15 00:40:08 -0300

    agrego funcionalidad de agregar todo's

---
+++ todo-app/todo-app.js
@@ -0,0 +1,14 @@
+const newTodo = document.querySelector("#new");
+const todoSection = document.querySelector("section");
+let todos = []; 
+
+function createTodo(){
+    console.log("new todo");
+    let todo = newTodo.value;
+    console.log(todo);
+    todos.push('<div><input type="checkbox" name="" id="">' + todo + ' <button>remove</button></div>');
+    console.log(todos);
+    console.log(todos.length);
+    todoSection.innerHTML += todos[todos.length-1];
+}
+newTodo.addEventListener("change", createTodo);  
